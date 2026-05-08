03ab7165abeaa6d2a43f57410ae84ac0f5859528
gis-carbonx <gis@carbonxco.com>
2026-05-08 16:01:56 +0700

    Update update_sheet.py

--- update_sheet.py
+++ update_sheet.py
@@ -63,7 +63,7 @@ def fetch_gfw_data(aoi_geom_dict):
 
     today = datetime.now(wib).strftime("%Y-%m-%d")
 
-    start_date = "2023-01-01"
+    start_date = "2025-01-01"
 
     sql = f"""
     SELECT
